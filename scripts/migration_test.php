3f6d40de3faf685cc70e9ee118c03b78f1aaca51
Princesse Patate <lea.regoudis@gmail.com>
2025-06-30 12:31:48 +0200

    PRIVI ROOT

--- scripts/migration_test.php
+++ scripts/migration_test.php
@@ -132,6 +132,7 @@ ALTER TABLE `useranswers`
   ADD CONSTRAINT `useranswers_ibfk_2` FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `useranswers_ibfk_3` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`) ON DELETE CASCADE;
 
+GRANT ALL PRIVILEGES ON `$db`.* TO '$user'@'localhost';
 SQL;
 
     $pdo->exec($sql);
